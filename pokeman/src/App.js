8eb53c0990e4e6f18abc6564471e4fdbfb3ccdb0
Kiruidyno <kirui472@gmail.com>
2023-05-07 00:29:08 +0300

    updated components

--- pokeman/src/App.js
+++ pokeman/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Main from './components/Main';
 import './components/style.css'
+import SearchBar from './components/SearchBar';
+
 function App() {
   return (
     <>
